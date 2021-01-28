ee939d4070c2b3ef4e6b993769e62f0c080a9831
rlacs4g <lacson.rafael@gmail.com>
2021-01-28 23:42:10 +0000

    init commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,32 +1,125 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function Home() {
 	const [currentTodo, setCurrentTodo] = useState("");
-	const [list, setList] = useState([
-		{ label: "Shave", done: true },
-		{ label: "Watch Cobra Kai", done: false },
-		{ label: "Watch Karate Kid", done: false },
-		{ label: "Lift weights", done: false }
-	]);
+	const [list, setList] = useState([]);
+	// Creation of list PUT
+	//----------------------------------------------------------------------
+	useEffect(
+		() => {
+			syncData();
+		},
 
+		[]
+	);
+	//----------------------------------------------------------------------
+	const syncData = () => {
+		return fetch("https://assets.breatheco.de/apis/fake/todos/user/rlacs4g")
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				setList(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+	//----------------------------------------------------------------------
 	const handleKeyPress = e => {
 		if (e.key === "Enter") {
-			setList(list.concat({ label: currentTodo, done: false }));
+			let newList = list.concat({ label: currentTodo, done: false });
+			updateTodo(newList);
 			setCurrentTodo("");
 		}
 	};
-
+	//----------------------------------------------------------------------
 	const deleteTodo = index => setList(list.filter((item, i) => i !== index));
-
+	//----------------------------------------------------------------------
 	const handleCompleteTodo = index => {
 		let newList = [].concat(list);
 		newList[index].done = !newList[index].done;
 
 		setList(newList);
 	};
+	//----------------------------------------------------------------------
+	const updateTodo = list => {
+		console.log("list", list);
+		return fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/rlacs4g",
+			{
+				method: "PUT",
+				body: JSON.stringify(list),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then(resp => {
+				if (!resp.ok) throw new Error(resp.statusText);
+				return resp.json();
+			})
+			.then(data => {
+				syncData();
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	};
 
+	//----------------------------------------------------------------------
+	const deleteTodoList = todolist => {
+		return fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/rlacs4g",
+			{
+				method: "delete",
+				body: JSON.stringify(todolist),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				console.log(data);
+				createTodoList();
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	};
+	//----------------------------------------------------------------------
+	const createTodoList = todolist => {
+		return fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/rlacs4g",
+			{
+				method: "post",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				console.log(data);
+				syncData();
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	};
+
+	//----------------------------------------------------------------------
 	return (
-		<div className="d-flex flex-column align-items-center justify-content-center wrap">
+		<div className="d-flex flex-column align-items-center justify-content-center">
 			<h1 className="mb-4">todos</h1>
 			<div className="todo-container">
 				<ul className="list-group">
@@ -65,6 +158,11 @@ export function Home() {
 					))}
 				</ul>
 				<div className="list-group-item footer">
+					<p
+						className="clear mr-4 d-inline"
+						onClick={() => deleteTodoList()}>
+						Clear All
+					</p>
 					{list.length > 0
 						? `${list.length} task${
 								list.length > 1 ? "s" : ""
